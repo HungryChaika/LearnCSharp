41832f8c2ac268d2b6e59d956c713f32c0d8829c
HungryChaika <vologzhanin_00@mail.ru>
2022-12-30 01:27:30 +0400

    Completed lesson4 (MyList)

---
+++ lesson4/MyList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace lesson4
+{
+    public class MyList<T>
+    {
+        private int _Count = 0;
+        public int Count
+        {
+            get => _Count;
+        }
+        T[] Elems = new T[0];
+        public T this[int Index]
+        {
+            get => Elems[Index];
+            set => Elems[Index] = value;
+        }
+
+        public void Add(T Item)
+        {
+            if(Elems.Length == _Count)
+            {
+                Array.Resize(ref Elems, Elems.Length + 1);
+                Elems[_Count] = Item;
+                _Count++;
+            }
+        }
+        public void DeleteByIndex(int Index)
+        {
+            if (Index >= 0 && Index < Elems.Length)
+            {
+                T[] Arr = new T[Elems.Length - 1];
+                int IndexForNewArr = 0;
+                int IndexForOldArr = 0;
+                while (true)
+                {
+                    if (IndexForOldArr != Index)
+                    {
+                        Arr[IndexForNewArr] = Elems[IndexForOldArr];
+                        IndexForNewArr++;
+                    }
+                    IndexForOldArr++;
+                    if(IndexForOldArr > Elems.Length - 1)
+                    {
+                        break;
+                    }
+                }
+                Array.Clear(Elems, 0, Elems.Length);
+                Array.Resize(ref Elems, Arr.Length);
+                Arr.CopyTo(Elems, 0);
+                _Count--;
+            }
+        }
+        public void DeleteByElement(T Item)
+        {
+            int Index = Array.IndexOf(Elems, Item);
+            if (Index != -1)
+            {
+                T[] Arr = new T[Elems.Length - 1];
+                int IndexForNewArr = 0;
+                int IndexForOldArr = 0;
+                while (true)
+                {
+                    if (IndexForOldArr != Index)
+                    {
+                        Arr[IndexForNewArr] = Elems[IndexForOldArr];
+                        IndexForNewArr++;
+                    }
+                    IndexForOldArr++;
+                    if (IndexForOldArr > Elems.Length - 1)
+                    {
+                        break;
+                    }
+                }
+                Array.Clear(Elems, 0, Elems.Length);
+                Array.Resize(ref Elems, Arr.Length);
+                Arr.CopyTo(Elems, 0);
+                _Count--;
+            }
+        }
+    }
+}
---
+++ lesson4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lesson4
+{
+    class Programm
+    {
+        public static void Main(string[] args)
+        {
+            MyList<int> Result = new MyList<int>();
+            Console.WriteLine($"Проверка.\n\nРазмер: {Result.Count}\nДобавление 3-х элементов.");
+            Result.Add(1);
+            Result.Add(5);
+            Result.Add(90);
+            WriteResult(Result);
+            Console.WriteLine($"Размер: {Result.Count}\nУдаление по индесу (1).");
+            Result.DeleteByIndex(1);
+            Console.WriteLine($"Размер: {Result.Count}");
+            WriteResult(Result);
+            Console.WriteLine("Удаление по значению (90).");
+            Result.DeleteByElement(90);
+            Console.WriteLine($"Размер: {Result.Count}");
+            WriteResult(Result);
+        }
+        static void WriteResult<T>(MyList<T> Result)
+        {
+            for (int i = 0; i < Result.Count; i++)
+            {
+                Console.WriteLine($"{i}: {Result[i]}");
+            }
+        }
+    }
+}
