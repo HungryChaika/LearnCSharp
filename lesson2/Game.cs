0779ddbaf0497f590deeafc6610cdb62a7c34dd9
HungryChaika <vologzhanin_00@mail.ru>
2022-12-29 13:30:28 +0400

    division into classes

---
+++ lesson2/Game.cs
@@ -0,0 +1,207 @@
+﻿using System;
+
+namespace lesson2
+{
+    public class Game
+    {
+        string _SymbolWalkingPlayer = "X";
+        public string SymbolWalkingPlayer
+        {
+            get => _SymbolWalkingPlayer;
+            set => _SymbolWalkingPlayer = value;
+        }
+        public int GameTableSideSize { get; set; }
+        public string[] GameTable;
+        public Game(int gameTableSideSize)
+        {
+            GameTableSideSize = gameTableSideSize;
+            GameTable = new string[gameTableSideSize * gameTableSideSize];
+            for (int i = 0; i < GameTableSideSize * gameTableSideSize; i++)
+            {
+                GameTable[i] = Convert.ToString(i + 1);
+            }
+        }
+        int _PlayerNumber = 0;
+        public int PlayerNumber
+        {
+            get => _PlayerNumber;
+            set => _PlayerNumber = value;
+        }
+
+        public void Move()
+        {
+            while (true)
+            {
+                Console.Write($"\nВведите номер, расположенный на той клетке, в которую вы хотите сходить.\nХодят {SymbolWalkingPlayer} :");
+                string elem = Console.ReadLine();
+                try
+                {
+                    PlayerNumber = Convert.ToInt32(elem);
+                    if (PlayerNumber > 0 && PlayerNumber <= GameTableSideSize * GameTableSideSize
+                        && GameTable[PlayerNumber - 1] != "X" && GameTable[PlayerNumber - 1] != "O")
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine($"Вводите пожалуйста числа из таблицы, а не вот это вот '{elem}'");
+                }
+            }
+            GameTable[PlayerNumber - 1] = SymbolWalkingPlayer;
+        }
+        public bool CheckingWinner()
+        {
+            int TopAndButtonCount = 0;
+            int LeftAndRightCount = 0;
+            int TopLeft_ButtomRightAndReversCount = 0;
+            int TopRight_ButtomLeftAndReversCount = 0;
+            for (int i = 1; i <= GameTableSideSize; i++)
+            {
+                //Top
+                if (PlayerNumber - 1 - GameTableSideSize * i >= 0)
+                {
+                    if (GameTable[PlayerNumber - 1 - GameTableSideSize * i] == SymbolWalkingPlayer)
+                    {
+                        TopAndButtonCount++;
+                    }
+                    else
+                    {
+                        TopAndButtonCount--;
+                    }
+                }
+                //Buttom
+                if (PlayerNumber - 1 + GameTableSideSize * i < GameTableSideSize * GameTableSideSize)
+                {
+                    if (GameTable[PlayerNumber - 1 + GameTableSideSize * i] == SymbolWalkingPlayer)
+                    {
+                        TopAndButtonCount++;
+                    }
+                    else
+                    {
+                        TopAndButtonCount--;
+                    }
+                }
+                //Left
+                if (PlayerNumber - 1 - i >= (PlayerNumber / GameTableSideSize - (PlayerNumber / GameTableSideSize) % 1 + (PlayerNumber % GameTableSideSize != 0 ? 1 : 0)) * GameTableSideSize - GameTableSideSize)
+                {
+                    if (GameTable[PlayerNumber - 1 - i] == SymbolWalkingPlayer)
+                    {
+                        LeftAndRightCount++;
+                    }
+                    else
+                    {
+                        LeftAndRightCount--;
+                    }
+                }
+                //Right
+                if (PlayerNumber - 1 + i < (PlayerNumber / GameTableSideSize - (PlayerNumber / GameTableSideSize) % 1 + (PlayerNumber % GameTableSideSize != 0 ? 1 : 0)) * GameTableSideSize)
+                {
+                    if (GameTable[PlayerNumber - 1 + i] == SymbolWalkingPlayer)
+
+                    {
+                        LeftAndRightCount++;
+                    }
+                    else
+                    {
+                        LeftAndRightCount--;
+                    }
+                }
+                //Diagonals
+                if ((PlayerNumber - 1) % (GameTableSideSize + 1) == 0 || (PlayerNumber - 1) % (GameTableSideSize - 1) == 0)
+                {
+                    //TopLeft <-> ButtomRight (\) ->
+                    if (PlayerNumber - 1 + (GameTableSideSize + 1) * i < GameTableSideSize * GameTableSideSize)
+                    {
+                        if (GameTable[PlayerNumber - 1 + (GameTableSideSize + 1) * i] == SymbolWalkingPlayer)
+                        {
+                            TopLeft_ButtomRightAndReversCount++;
+                        }
+                        else
+                        {
+                            TopLeft_ButtomRightAndReversCount--;
+                        }
+                    }
+                    //TopLeft <-> ButtomRight (\) <-
+                    if (PlayerNumber - 1 - (GameTableSideSize + 1) * i >= 0)
+                    {
+                        if (GameTable[PlayerNumber - 1 - (GameTableSideSize + 1) * i] == SymbolWalkingPlayer)
+                        {
+                            TopLeft_ButtomRightAndReversCount++;
+                        }
+                        else
+                        {
+                            TopLeft_ButtomRightAndReversCount--;
+                        }
+                    }
+                    //TopRight <->ButtomLeft(/) <-
+                    if (PlayerNumber - 1 + (GameTableSideSize - 1) * i < GameTableSideSize * GameTableSideSize - 1)
+                    {
+                        if (GameTable[PlayerNumber - 1 + (GameTableSideSize - 1) * i] == SymbolWalkingPlayer)
+                        {
+                            TopRight_ButtomLeftAndReversCount++;
+                        }
+                        else
+                        {
+                            TopRight_ButtomLeftAndReversCount--;
+                        }
+                    }
+                    //TopRight <->ButtomLeft(/) ->
+                    if (PlayerNumber - 1 - (GameTableSideSize - 1) * i > 0)
+                    {
+                        if (GameTable[PlayerNumber - 1 - (GameTableSideSize - 1) * i] == SymbolWalkingPlayer)
+                        {
+                            TopRight_ButtomLeftAndReversCount++;
+                        }
+                        else
+                        {
+                            TopRight_ButtomLeftAndReversCount--;
+                        }
+                    }
+                }
+            }
+
+            if (TopAndButtonCount == GameTableSideSize - 1 ||
+               LeftAndRightCount == GameTableSideSize - 1 ||
+               TopLeft_ButtomRightAndReversCount == GameTableSideSize - 1 ||
+               TopRight_ButtomLeftAndReversCount == GameTableSideSize - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CheckingUnclean()
+        {
+            int MaxCountElems = GameTableSideSize * GameTableSideSize;
+            for (int i = 0; i < GameTableSideSize * GameTableSideSize; i++)
+            {
+                if (GameTable[i] == "X" || GameTable[i] == "O")
+                {
+                    MaxCountElems--;
+                }
+            }
+            if (MaxCountElems == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void ChangePlayer()
+        {
+            if (SymbolWalkingPlayer == "X")
+            {
+                SymbolWalkingPlayer = "O";
+            }
+            else
+            {
+                SymbolWalkingPlayer = "X";
+            }
+        }
+    }
+}
---
+++ lesson2/UI.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace lesson2
+{
+    public class UI
+    {
+        public void Render(string[] GameTable, int GameTableSideSize, string SymbolWalkingPlayer, int PlayerNumber)
+        {
+            int NumberForTable = 0;
+            int Counter = 2;
+            int DigitNumber = (Convert.ToString(GameTableSideSize * GameTableSideSize)).Length;
+            for (int i = 0; i < GameTableSideSize * 2 + 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int t = 0; t < (DigitNumber * 2 + 1 + 1) * GameTableSideSize + 1; t++)
+                    {
+                        Console.Write("-");
+                    }
+                    Counter = 2;
+                }
+                else
+                {
+                    for (int j = 0; j < GameTableSideSize * (4 + DigitNumber - 1) + 1; j++)
+                    {
+                        if (j % (4 + DigitNumber - 1) == 0)
+                        {
+                            if (j == 0)
+                            {
+                                Console.Write("\n");
+                            }
+                            Console.Write("|");
+                            if (j == GameTableSideSize * (4 + DigitNumber - 1))
+                            {
+                                Console.Write("\n");
+                            }
+                        }
+                        else
+                        {
+                            if (j == Counter)
+                            {
+                                int NumberOfSpaces = DigitNumber - GameTable[NumberForTable].Length;
+                                for (int k = 0; k < NumberOfSpaces; k++)
+                                {
+                                    Console.Write(" ");
+                                }
+                                if (PlayerNumber != 0 && NumberForTable == PlayerNumber - 1)
+                                {
+                                    SetCollor(SymbolWalkingPlayer);
+                                }
+                                Console.Write(GameTable[NumberForTable]);
+                                Counter += 4 + DigitNumber - 1;
+                                NumberForTable++;
+                                SetCollor();
+                            }
+                            else
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        void SetCollor(string elem = " ")
+        {
+            if (elem == "X")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                return;
+            }
+            if (elem == "O")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                return;
+            }
+            if (elem == " ")
+            {
+                Console.ResetColor();
+                return;
+            }
+        }
+    }
+}
